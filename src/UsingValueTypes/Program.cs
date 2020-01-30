764a84f1273c97d5e9820015cc890e50765cb5a6
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-01-29 20:13:31 -0500

    First commit.

---
+++ src/UsingValueTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
