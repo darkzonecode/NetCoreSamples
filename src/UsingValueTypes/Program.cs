0ab0a14792de3fd48a0d89eb35b2286b3bebd748
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-02-01 08:04:21 -0500

    Create Value Type Structure.

--- src/UsingValueTypes/Program.cs
+++ src/UsingValueTypes/Program.cs
@@ -7,6 +7,57 @@ namespace UsingValueTypes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Cycle ttt = new Cycle(0, 369);
+
+
+            ttt.Value = 500;
+
+            var test = ttt.Value;
+
+           
         }
     }
+
+
+    struct Cycle
+    {
+        // Private Fields.
+        int _val, _min, _max;
+
+        //Constructor
+        public Cycle(int min, int max)
+        {
+            // Must Initialise all declared values, else error, try to commet initialization.
+            _val = min;
+            _min = min;
+            _max = max;
+        }
+
+        public int Value
+        {
+            get { return _val; }
+            set
+            {
+                if (value > _max)
+                {
+                    this.Value = value - _max + _min - 1;
+                }
+                else
+                {
+                    if (value <_min)
+                    {
+                        this.Value = _min - value + _max - 1;
+                    }
+                    else
+                    {
+                        _val = value;
+                    }
+                }
+            }
+        }
+
+    }
+
+
 }
