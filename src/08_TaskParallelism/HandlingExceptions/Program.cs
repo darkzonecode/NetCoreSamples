a3d8281e593db630c66905ddd04263f4e3130c53
Andriy Kostiv <AndriyKostiv@outlook.com>
2021-04-08 14:03:23 -0400

    Add Handling Exceptions sample.

--- src/08_TaskParallelism/HandlingExceptions/Program.cs
+++ src/08_TaskParallelism/HandlingExceptions/Program.cs
@@ -24,16 +24,53 @@ namespace HandlingExceptions
             {
                 Console.WriteLine($"Task has finished with exception {ex.InnerException.Message}");
             }
-            Console.ReadLine();
+            Console.WriteLine("\nFinish error handling for single task.");
 
             // **************************************************************************************
 
             // Handling exceptions from multiple tasks.
+            Console.WriteLine("\nStart error handling for multiple task.");
+            Task taskA = Task.Factory.StartNew(() => throw new DivideByZeroException());
+            Task taskB = Task.Factory.StartNew(() => throw new ArithmeticException());
+            Task taskC = Task.Factory.StartNew(() => throw new NullReferenceException());
+            try
+            {
+                Task.WaitAll(taskA, taskB, taskC);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception innerException in ex.InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+
+            Console.WriteLine("\nFinish error handling for multiple task.");
+
+            // *****************************************************************************************
+
+            // Handling task exceptions with a callback function.
+            Console.WriteLine("\nStart error handling for multiple task using callback function.");
+            Task taskA1 = Task.Factory.StartNew(() => throw new DivideByZeroException());
+            Task taskB1 = Task.Factory.StartNew(() => throw new ArithmeticException());
+            Task taskC1 = Task.Factory.StartNew(() => throw new NullReferenceException());
+            try
+            {
+                Task.WaitAll(taskA1, taskB1, taskC1);
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle(innerException =>
+                {
+                    Console.WriteLine(innerException.Message);
+                    return true;
+                }
+                );
 
 
+            }
 
-
-
+            Console.WriteLine("\nFinish error handling for multiple task using callback.");
         }
     }
 }
