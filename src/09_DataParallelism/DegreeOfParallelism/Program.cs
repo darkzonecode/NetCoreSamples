118dfca7f801b9a15b1014a63fdcc7e98b65746c
Andriy Kostiv <AndriyKostiv@outlook.com>
2021-04-09 15:30:50 -0400

    Add Partitioner demo.

--- src/09_DataParallelism/DegreeOfParallelism/Program.cs
+++ src/09_DataParallelism/DegreeOfParallelism/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DegreeOfParallelism
 {
@@ -6,7 +8,33 @@ namespace DegreeOfParallelism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Degree of parallelism!");
+
+            // DegreeOfParallelismWithParallelFor();
+            DegreeOfParallelismWithParallelForEach();
+
+
+
+
+
+        }
+
+
+        private static void DegreeOfParallelismWithParallelForEach()
+        {
+            var items = Enumerable.Range(1, 20);
+            Parallel.ForEach(items, new ParallelOptions { MaxDegreeOfParallelism = 4 }, item =>
+            {
+                Console.WriteLine($"Index {item} executing on Task Id {Task.CurrentId}");
+            });
+        }
+
+        private static void DegreeOfParallelismWithParallelFor()
+        {
+            Parallel.For(1, 20, new ParallelOptions { MaxDegreeOfParallelism = 4 }, index =>
+            {
+                Console.WriteLine($"Index {index} executing on Task Id {Task.CurrentId}");
+            });
         }
     }
 }
---
+++ src/09_DataParallelism/PartitionerDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartitionerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello PartitionerDemo!");
+
+            CustomPartitionDemo();
+        }
+
+
+        private static void CustomPartitionDemo()
+        {
+            var source = Enumerable.Range(1, 100).ToList();
+            OrderablePartitioner<Tuple<int, int>> orderablePartitioner = Partitioner.Create(1, 100);
+
+            Parallel.ForEach(orderablePartitioner, (range, state) =>
+            {
+                var startRange = range.Item1;
+                var endRange = range.Item2;
+                Console.WriteLine($"Range execution finished on task {Task.CurrentId} with range {startRange}-{endRange}");
+            });
+        }
+    }
+}
